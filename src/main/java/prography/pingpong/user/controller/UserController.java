6f2c71c8d20b075b4c335956f048875954a3f4b7
csct3434 <csct3434@gmail.com>
2024-02-01 13:24:22 +0900

    feat: 유저 전체 조회 API 구현

---
+++ src/main/java/prography/pingpong/user/controller/UserController.java
@@ -0,0 +1,31 @@
+package prography.pingpong.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import prography.pingpong.common.response.ApiResponse;
+import prography.pingpong.user.dto.FindAllUsersCommand;
+import prography.pingpong.user.dto.FindAllUsersResponse;
+import prography.pingpong.user.service.FindAllUsersService;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final FindAllUsersService findAllUsersService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<FindAllUsersResponse>> findAllUsers(
+        @RequestParam("size") int pageSize,
+        @RequestParam("page") int pageNumber
+    ) {
+        FindAllUsersCommand command = new FindAllUsersCommand(pageSize, pageNumber);
+        FindAllUsersResponse result = findAllUsersService.doService(command);
+        return ResponseEntity.ok(ApiResponse.success(result));
+    }
+
+}
