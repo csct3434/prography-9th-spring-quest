63023a28645036617c8ab522da5e691feb88c64c
csct3434 <csct3434@gmail.com>
2024-02-01 15:05:10 +0900

    feat: 방 생성 서비스 인터페이스 작성

---
+++ src/main/java/prography/pingpong/room/dto/CreateRoomCommand.java
@@ -0,0 +1,26 @@
+package prography.pingpong.room.dto;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import prography.pingpong.common.exception.RestApiException;
+import prography.pingpong.room.domain.room.RoomType;
+
+@Getter
+public class CreateRoomCommand {
+
+    private final int userId;
+    private final RoomType roomType;
+    private final String title;
+
+    public CreateRoomCommand(int userId, String roomType, String title) {
+        this.userId = userId;
+        this.title = title;
+
+        // 일치하는 RoomType이 없으면 201 응답
+        try {
+            this.roomType = RoomType.valueOf(roomType);
+        } catch (Exception e) {
+            throw RestApiException.badRequest(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
