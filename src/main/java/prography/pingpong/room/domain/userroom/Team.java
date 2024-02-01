af0a79255e40988ca5b1300102d0a3765a3086a9
csct3434 <csct3434@gmail.com>
2024-02-01 14:11:12 +0900

    feat: UserRoom 엔티티 작성

---
+++ src/main/java/prography/pingpong/room/domain/userroom/Team.java
@@ -0,0 +1,5 @@
+package prography.pingpong.room.domain.userroom;
+
+public enum Team {
+    RED, BLUE
+}
---
+++ src/main/java/prography/pingpong/room/domain/userroom/UserRoom.java
@@ -0,0 +1,40 @@
+package prography.pingpong.room.domain.userroom;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import prography.pingpong.room.domain.room.Room;
+import prography.pingpong.user.domain.User;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Room room;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private Team team;
+
+    public UserRoom(Room room, User user, Team team) {
+        this.room = room;
+        this.user = user;
+        this.team = team;
+    }
+}
