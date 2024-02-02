f160c3efe5b8a8ae6658cda95ff21c953eb6712c
csct3434 <csct3434@gmail.com>
2024-02-03 01:17:52 +0900

    chore: 예외 발생 시, 응답 코드를 200으로 고정
    - 요구사항 변경에 의한 코드 수정

--- src/main/java/prography/pingpong/common/exception/GlobalExceptionHandler.java
+++ src/main/java/prography/pingpong/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package prography.pingpong.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,19 +11,18 @@ import prography.pingpong.common.response.ApiResponse;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RestApiException.class)
-    public ResponseEntity<ApiResponse<Void>> handleCustomException(RestApiException e) {
+    public ResponseEntity<ApiResponse<Void>> handleRestApiException(RestApiException e) {
         log.info(e.getMessage());
-        return buildResponse(e.getHttpStatus(), e.getApiResponse());
+        return buildResponse(e.getApiResponse());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleUnexpectedException(Exception e) {
         log.error(e.getMessage());
-        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, ApiResponse.error());
+        return buildResponse(ApiResponse.error());
     }
 
-    private ResponseEntity<ApiResponse<Void>> buildResponse(HttpStatus httpStatus, ApiResponse<Void> apiResponse) {
-        return ResponseEntity.status(httpStatus)
-            .body(apiResponse);
+    private ResponseEntity<ApiResponse<Void>> buildResponse(ApiResponse<Void> apiResponse) {
+        return ResponseEntity.ok(apiResponse);
     }
 }
--- src/main/java/prography/pingpong/common/exception/RestApiException.java
+++ src/main/java/prography/pingpong/common/exception/RestApiException.java
@@ -3,21 +3,15 @@ package prography.pingpong.common.exception;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 import prography.pingpong.common.response.ApiResponse;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class RestApiException extends RuntimeException {
 
-    private final HttpStatus httpStatus;
     private final ApiResponse<Void> apiResponse;
 
     public static RestApiException badRequest() {
-        return new RestApiException(HttpStatus.BAD_REQUEST, ApiResponse.badRequest());
-    }
-
-    public static RestApiException badRequest(HttpStatus httpStatus) {
-        return new RestApiException(httpStatus, ApiResponse.badRequest());
+        return new RestApiException(ApiResponse.badRequest());
     }
 }
