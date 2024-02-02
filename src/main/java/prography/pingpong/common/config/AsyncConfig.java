a076580c1fd14039a99645c81024b65b75d62504
csct3434 <csct3434@gmail.com>
2024-02-03 07:40:14 +0900

    feat: @EnableAsync 설정

---
+++ src/main/java/prography/pingpong/common/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package prography.pingpong.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+}
