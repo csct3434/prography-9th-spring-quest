c835a4a4998a19fbbae4bd4303e774dee59a3679
csct3434 <csct3434@gmail.com>
2024-02-03 04:01:10 +0900

    refactor: 방 생성 API 입력 유효성 개선
    - userId : 값에 대한 검증은 도메인 규칙이므로 커맨드 객체에서 수행한다
    - roomType : @NotBlank에는 Not Null이 포함되어 있다
    - title : 제목에 대한 별도의 규칙이 없으므로, 우선 공백은 허용한다

--- src/main/java/prography/pingpong/room/dto/CreateRoomRequest.java
+++ src/main/java/prography/pingpong/room/dto/CreateRoomRequest.java
@@ -1,8 +1,8 @@
 package prography.pingpong.room.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,15 +14,13 @@ import lombok.Setter;
 public class CreateRoomRequest {
 
     @NotNull
-    @Positive
     private Integer userId;
 
-    @NotNull
     @NotBlank
     private String roomType;
 
     @NotNull
-    @NotBlank
+    @NotEmpty
     private String title;
 
     public CreateRoomCommand buildCommand() {
